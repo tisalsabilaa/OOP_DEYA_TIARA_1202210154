8b853bc02815c096c9d3dd028b5ff026c737d3dc
tisalsabilaa <tisalsabilaaa@gmail.com>
2022-11-05 07:08:56 +0700

    Modul1

--- src/Menu.java
+++
@@ -1,5 +0,0 @@
-public class Menu {
-
-    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
-
-}
--- src/User.java
+++
@@ -1,8 +0,0 @@
-public class User {
-
-    // TODO Create Attribute of User; Name and Phone Number then Create Setter
-
-    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
-
-
-}
