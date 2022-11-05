9f891421ba8af947aa5e3aee36b9534264d81a10
tisalsabilaa <tisalsabilaaa@gmail.com>
2022-11-05 08:07:18 +0700

    Modul 1

---
+++ src/Modulpertama.java
@@ -0,0 +1,19 @@
+package OOP_DEYA_TIARA_1202210154;
+
+import java.util.Scanner;
+public class Modulpertama {
+    public static void main(String[] args) {
+        System.out.println("Selamat datang di Restoran EAD");
+        System.out.println("Silahkan Register terlebih dahulu\n============================");
+        
+        String nama ;
+        int nohp ;
+        Scanner nama = new Scanner(System.in);
+        Scanner nohp = new Scanner(System.in);
+        String nama = Scanner.nextline();
+        System.out.println("Register Sukses\n"+nama+"\n"+nohp);
+
+
+
+    }
+}
